8555f79053696ee9640da7821864f47d8a050b4b
maxahn <ahnmaximus@gmail.com>
2016-06-13 18:05:53 -0700

    added custom css file to center the content as well as added some gifs

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,32 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+  @tracks = Track.all
+  erb :'/tracks/index'
+end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'/tracks/index'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @track.save 
+    redirect '/tracks'
+  else
+    erb :'/tracks/new'
+  end
+end
+
+get '/tracks/new' do
+  erb :'tracks/new'
+end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
 end
